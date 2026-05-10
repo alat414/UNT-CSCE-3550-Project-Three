2b955fe876adf28b4b38e9fe818a0cf283181c0b
alat414 <gustavo.alatriste@g.austincc.edu>
2026-05-10 18:48:12 -0500

    created crypto.js file for password hashing

---
+++ utils/crypto.js
@@ -0,0 +1,10 @@
+/** *******************************************************
+ * Name: Gustavo Alatriste
+*  Assignment: JWKS server with SQLite, AES key, and
+*              hashing integrated
+*  Purpose: A separate utility file for password
+*           hashing for the current JWKS server (crypto.js) 
+* **********************************************************/
+
+const crypto = require('crypto');
+
