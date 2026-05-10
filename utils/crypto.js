91bfb2bcd06c8a43ec2b66bed553975741baecaa
alat414 <gustavo.alatriste@g.austincc.edu>
2026-05-10 18:51:42 -0500

    Adding hash password function

--- utils/crypto.js
+++ utils/crypto.js
@@ -8,3 +8,14 @@
 
 const crypto = require('crypto');
 
+/** The following is a helper function to 
+ * hash passwords in proper format
+ * 
+ * @param {*} password 
+ * @returns A SHA 256 cryptographic hash of the given
+ *          parameter as a hexadecimal string.
+ */
+function hashPasswordSHA256(password)
+{
+    return crypto.createHast('sha256').update(password).digest('hex');
+}
