b69518d672b693402a2c6049788bdbddc644c3ba
alat414 <gustavo.alatriste@g.austincc.edu>
2026-05-10 19:43:14 -0500

    importing correct modules, adding first generating value using basic uuid v4 calls

--- utils/passwordGenerator.js
+++ utils/passwordGenerator.js
@@ -6,3 +6,15 @@
 *           generating functions for the 
 *           current JWKS server (crypto.js) 
 * ****************************************************/
+
+const crypto = require('crypto');
+const { v4: uuid4} = require('uuid');
+
+class passwordGenerator
+{
+    static uuidOnly(removeHyphens = true)
+    {
+        const pwd = uuid4();
+        return removeHyphens ? pwd.replace(/-/g, '') : pwd;
+    }
+}
