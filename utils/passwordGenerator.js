b9f9ed146993f3ba40ae6d28eb0784edd7fbc010
alat414 <gustavo.alatriste@g.austincc.edu>
2026-05-10 19:39:42 -0500

    file created for password generating

---
+++ utils/passwordGenerator.js
@@ -0,0 +1,8 @@
+/** **************************************************
+ * Name: Gustavo Alatriste
+*  Assignment: JWKS server with SQLite, AES key, 
+*              and hashing integrated
+*  Purpose: A separate  file for password
+*           generating functions for the 
+*           current JWKS server (crypto.js) 
+* ****************************************************/
